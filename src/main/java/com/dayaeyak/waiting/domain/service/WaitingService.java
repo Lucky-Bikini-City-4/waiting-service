e66cad82c34dbeaa7c1be53ebf03f0668488ab2c
dayaelee <dayae.dev@gmail.com>
2025-09-10 21:23:35 +0900

    feat: 웨이팅 단건, 레스토랑 id값으로 전체 조회, 삭제, 레스토랑 id값으로 삭제 하는 비즈니스 로직 추가

--- src/main/java/com/dayaeyak/waiting/domain/service/WaitingService.java
+++ src/main/java/com/dayaeyak/waiting/domain/service/WaitingService.java
@@ -2,21 +2,26 @@ package com.dayaeyak.waiting.domain.service;
 
 import com.dayaeyak.waiting.domain.dto.request.WaitingCreateRequestDto;
 import com.dayaeyak.waiting.domain.dto.response.WaitingCreateResponseDto;
+import com.dayaeyak.waiting.domain.dto.response.WaitingListResponseDto;
 import com.dayaeyak.waiting.domain.dto.response.WaitingResponseDto;
 import com.dayaeyak.waiting.domain.entity.Waiting;
 import com.dayaeyak.waiting.domain.enums.WaitingStatus;
 import com.dayaeyak.waiting.domain.repository.jpa.WaitingRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-
 
 
 import java.math.BigInteger;
+import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class WaitingService {
 
@@ -39,5 +44,38 @@ public class WaitingService {
         Waiting waiting = waitingRepository.findById(waitingId)
                 .orElseThrow(() -> new EntityNotFoundException());
         return WaitingResponseDto.from(waiting);
+    }
+
+    public WaitingListResponseDto getWaitings(
+            BigInteger restaurantId,
+            int page,
+            int size
+    ){
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+        Page<Waiting> result = waitingRepository.findByRestaurantIdAndDeletedAtIsNull(restaurantId, pageable);
+        List<WaitingResponseDto> data = result.getContent().stream()
+                .map(WaitingResponseDto::from)
+                .toList();
+
+        return new WaitingListResponseDto(result.getTotalElements(), data);
+    }
+
+    public void deleteWaiting(BigInteger waitingId){
+        Waiting waiting = waitingRepository.findById(waitingId)
+                .orElseThrow(() -> new EntityNotFoundException());
+
+        if (waiting.getDeletedAt() != null) return;
+        waiting.delete();
+    }
+
+    public void deleteWaitingAll(BigInteger restaurantId){
+        List<Waiting> waitingList = waitingRepository.findByRestaurantIdAndDeletedAtIsNull(restaurantId);
+        if (waitingList.isEmpty()) return;
+
+        for (Waiting waiting : waitingList){
+            if (waiting.getDeletedAt()==null) {
+                waiting.delete();
+            }
+        }
     }
 }
