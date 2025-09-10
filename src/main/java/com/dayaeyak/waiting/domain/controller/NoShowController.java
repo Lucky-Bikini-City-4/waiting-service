4d4ac6ecc90ec386cf8ef7e27e5da986f46a347d
dayaelee <dayae.dev@gmail.com>
2025-09-10 22:41:21 +0900

    feat: 노쇼 단건, 레스토랑 id값으로 전체 조회, 삭제, 레스토랑 id값으로 삭제 하는 컨트롤러 추가

---
+++ src/main/java/com/dayaeyak/waiting/domain/controller/NoShowController.java
@@ -0,0 +1,71 @@
+package com.dayaeyak.waiting.domain.controller;
+
+import com.dayaeyak.waiting.domain.dto.request.NoShowCreateRequestDto;
+import com.dayaeyak.waiting.domain.dto.request.WaitingCreateRequestDto;
+import com.dayaeyak.waiting.domain.dto.response.*;
+import com.dayaeyak.waiting.domain.service.NoShowService;
+import com.dayaeyak.waiting.utils.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+
+@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/noShows")
+public class NoShowController {
+
+    private final NoShowService noshowService;
+
+    // 노쇼 등록
+    @PostMapping
+    public ResponseEntity<ApiResponse<NoShowCreateResponseDto>> createNoShow(
+            @Validated
+            @RequestBody NoShowCreateRequestDto requestDto){
+        NoShowCreateResponseDto responseDto = noshowService.register(requestDto);
+        return ApiResponse.success(201, "노쇼유저가 등록되었습니다.", responseDto);
+    }
+
+
+    // 노쇼 단건 조회
+    @GetMapping("/{noShowId}")
+    public ResponseEntity<ApiResponse<NoShowResponseDto>> getNoShow(
+            @Validated
+            @PathVariable BigInteger noShowId){
+        NoShowResponseDto responseDto = noshowService.getNoShow(noShowId);
+        return ApiResponse.success(200, "노쇼유저가 조회되었습니다. ", responseDto);
+    }
+
+    // 가게별 웨이팅 목록 조회
+    @GetMapping
+    public ResponseEntity<ApiResponse<NoShowListResponseDto>> getNoShows(
+            @RequestParam BigInteger restaurantId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size) {
+        NoShowListResponseDto responseDto = noshowService.getNoShows(restaurantId, page, size);
+        return ApiResponse.success(200, "", responseDto);
+    }
+
+    // 웨이팅 단건 삭제
+    @DeleteMapping("/{noShowId}")
+    public ResponseEntity<ApiResponse<Void>> deleteNoShow(
+            @Validated
+            @PathVariable BigInteger noShowId){
+        noshowService.deleteNoShow(noShowId);
+        return ApiResponse.success(204, "노쇼유저가 삭되었습니다.", null);
+    }
+
+    // 웨이팅 모든 음식점에서 전체 삭제
+    @DeleteMapping("/all/{restaurantId}")
+    public ResponseEntity<ApiResponse<Void>> deleteNoShowAll(
+            @Validated
+            @PathVariable BigInteger restaurantId){
+        noshowService.deleteNoShowAll(restaurantId);
+        return ApiResponse.success(204, "노쇼유저가 삭되었습니다.", null);
+    }
+
+}
