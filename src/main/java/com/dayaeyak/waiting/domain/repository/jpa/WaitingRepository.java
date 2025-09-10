bc697b57564ecd1dcb1dcfd31e40464307557818
dayaelee <dayae.dev@gmail.com>
2025-09-10 21:22:29 +0900

    add: 웨이팅 등록, 삭제에 필요한 JPA 메서드 추가

--- src/main/java/com/dayaeyak/waiting/domain/repository/jpa/WaitingRepository.java
+++ src/main/java/com/dayaeyak/waiting/domain/repository/jpa/WaitingRepository.java
@@ -1,13 +1,19 @@
 package com.dayaeyak.waiting.domain.repository.jpa;
 
 import com.dayaeyak.waiting.domain.entity.Waiting;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @Repository
 public interface WaitingRepository extends JpaRepository<Waiting, BigInteger> {
     Waiting save(Waiting waiting);
+    Page<Waiting> findByRestaurantIdAndDeletedAtIsNull(BigInteger restaurantId, Pageable pageable);
+    List<Waiting> findByRestaurantIdAndDeletedAtIsNull(BigInteger restaurantId);
+
 }
 
